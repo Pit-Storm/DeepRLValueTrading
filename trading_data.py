bb891fcf30ad0a1b7c88fd946922ac765937bd00
Pit-Storm <33754750+Pit-Storm@users.noreply.github.com>
2020-11-12 19:15:35 +0000

    saved historical trading data for all used symbols and added output file to .gitignore

--- trading_data.py
+++ trading_data.py
@@ -11,17 +11,29 @@ import pandas as pd
 ###
 
 endpoint = "eod"
-symbol = "AAPL"
-exchange = "US"
+
+with open("stocks.txt") as file:
+    symb_exchange = file.read().splitlines()
+
+stocks = [item.split(".") for item in symb_exchange]
 
 params = {
     "period": "d",
     "order": "a"
 }
-
 # %%
-tmp = ehd.get_data(endpoint=endpoint, symbol=symbol, exchange=exchange, params=params)
-tmp_df = ehd.make_df(data=tmp, endpoint=endpoint)
-#%%
-tmp_df.describe()
+stocks_df = pd.DataFrame()
+for stock in stocks:
+    tmp = ehd.get_data(endpoint=endpoint, symbol=stock[0], exchange=stock[1], params=params)
+    tmp_df = ehd.make_df(data=tmp, endpoint=endpoint)
+    tmp_df["symbol"] = stock[0]
+    tmp_df = tmp_df.set_index(keys="symbol", append=True)
+    stocks_df = stocks_df.append(tmp_df)
+
+stocks_df = (stocks_df.sort_index()
+    .loc[("2000-01-03",):("2019-12-31",)]
+    .fillna(method="bfill")
+)
+# %%
+stocks_df.reset_index().to_csv("stocksdata_trading.csv")
 # %%

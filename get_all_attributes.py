c93b6d17ef3af055d53931134648ed9ff5616264
Pit-Storm <33754750+Pit-Storm@users.noreply.github.com>
2020-11-13 20:22:26 +0000

    joined and missing values of trading and funda data

--- get_all_attributes.py
+++ get_all_attributes.py
@@ -5,48 +5,50 @@
 
 import eodhistoricaldata as ehd
 import pandas as pd
+import seaborn as sns
 # %%
 ###
 # Reading Data
 ###
 
-exchange = "US"
-symbols = [
-    "AAPL"
-]
-endpoints = [
-    "eod", "fundamentals"
-]
-params = {
-    "period": "d",
-    "order": "a"
-}
-
-frames = {}
+trading_df = (pd.read_csv("stocksdata_trading.csv", parse_dates=["date"])
+    .set_index(["date","symbol"])
+    .drop("Unnamed: 0", axis=1)
+     .rename(columns={"adjusted_close":"adj_close"})
+)
+funda_df = (pd.read_csv("stocksdata_fundamental.csv", parse_dates=["date"])
+    .set_index(["date","symbol"])
+    .drop("Unnamed: 0", axis=1)
+    .rename(columns={"epsActual":"eps", "book_value": "bv", "shares": "shrs"})
+)
+# stocks_df = trading_df.join(funda_df)
 # %%
+# Handling trading NaN values
+trading_df = trading_df.fillna(0)
+
+# %%
+# Handle fundamental data NaN values
+symbols = funda_df.index.get_level_values(level="symbol")
+
 for symbol in symbols:
-    for endpoint in endpoints:
-        if endpoint == "eod":
-            tmp_eod = ehd.get_data(endpoint=endpoint, symbol=symbol, exchange=exchange, params=params)
-            tmp_eod_df = ehd.make_df(data=tmp_eod, endpoint=endpoint)
-        elif endpoint == "fundamentals":
-            # Get EPS
-            params["filter"] =  "Earnings::History"
-            tmp_eps = ehd.get_data(endpoint=endpoint, symbol=symbol, exchange=exchange, params=params)
-            tmp_eps_df = ehd.make_df(data=tmp_eps, endpoint=endpoint, call_filter=params["filter"])
-            # Get BV
-            params["filter"] = "Financials::Balance_Sheet::quarterly"
-            tmp_bv = ehd.get_data(endpoint=endpoint, symbol=symbol, exchange=exchange, params=params)
-            tmp_bv_df = ehd.make_df(data=tmp_bv, endpoint=endpoint, call_filter=params["filter"])
+    funda_df.loc[(slice(None),symbol),("bv")] = funda_df.loc[(slice(None),symbol),("bv")].interpolate(limit=4,limit_area="inside")
+    funda_df.loc[(slice(None),symbol),("shrs")] = funda_df.loc[(slice(None),symbol),("shrs")].interpolate(limit=4,limit_area="inside")
 
-    df_stock = tmp_eod_df.join([tmp_eps_df["epsActual"], tmp_bv_df["book_value"]])
-    df_stock["p_to_bv"] = df_stock["adjusted_close"] / df_stock["book_value"]
-    df_stock["p_to_e"] = df_stock["adjusted_close"] / df_stock["epsActual"]
-    df_stock.drop(columns=["book_value"], inplace=True)
-    df_stock.rename(columns={"epsActual":"eps","adjusted_close":"adj_close"}, inplace=True)
-    df_Stock.fillna(method="ffill", inplace=True)
-
-    frames[symbol] = df_stock.copy()
+funda_df = funda_df.fillna(0)
 # %%
-frames["AAPL"].describe()
+stocks_df = trading_df.join(funda_df)
+# %%
+# Handling missing values after joining
+# First forward fill the fundamental data, because we assume
+# that it doesn't change after it has been released first time
+stocks_df = stocks_df.fillna(method="ffill")
+# If there hasn't been a value before, fill with 0
+stocks_df = stocks_df.fillna(0)
+# %%
+stocks_df["p_to_bv"] = stocks_df["adj_close"] / stocks_df["bv"]
+stocks_df["p_to_e"] = stocks_df["adj_close"] / stocks_df["eps"]
+stocks_df.drop(columns=["bv"], inplace=True)
+
+# %%
+stocks_df.reset_index().to_csv("stocksdata_all.csv")
 # %%

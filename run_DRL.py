f428157ad0d91daf54d29b730f26322725add070
Pit-Storm <33754750+Pit-Storm@users.noreply.github.com>
2020-11-18 15:50:34 +0000

    first needed files

---
+++ run_DRL.py
@@ -0,0 +1,28 @@
+def main() -> None:
+    ###
+    # Define variables
+
+    ###
+    # Load Dataset
+
+    ###
+    # Setup ENV
+
+    ###
+    # Trainingloop
+
+        ###
+        # Train Model
+
+        ###
+        # Validate Training every n steps
+
+        ###
+        # If model has validation measurement over specific
+        # threshold stop training
+
+    ###
+    # 
+
+if __name__ == "__main__":
+    main()

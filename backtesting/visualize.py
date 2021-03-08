239e21c39a04b58697f34cd972d386bf21593dbc
Pit-Storm <33754750+Pit-Storm@users.noreply.github.com>
2021-03-08 12:16:47 +0000

    adjustment for new filenames and using totalValues for performance calculation

--- backtesting/visualize.py
+++ backtesting/visualize.py
@@ -9,24 +9,25 @@ import quantstats as qs
 # extend pandas functionality with metrics, etc.
 qs.extend_pandas()
 # %%
-algo_name = "BuyHold" # What algo to evaluate. must be the same as specified in config.py
-rewards_ser = None # timeseries with dates and rewards
+algo_name = "random" # What algo to evaluate. must be the same as specified in config.py
+values_ser = None # timeseries with dates and rewards
 portfolio_df = None # index (Dates), [symbols, ...] (float: net amount holding), cash (float)
-actions_df = None # index (date), amount (int: action), price (float: open), symbol (string)
 indices_df = None # Timeseries with dates and daily returns
 sum_mode = "comp" # How will the growth rate be calculated? Compound or Cummulative
 
-algo_dir = Path.cwd().parent / "results" / algo_name
+algo_dir = Path.cwd().parent / "results" / algo_name.upper()
+# get all rundirs
 run_dirs = [dr for dr in algo_dir.iterdir() if dr.is_dir()]
-ep_path = run_dirs[-1] / "env_info" / "test" / "episode_0001.json"
+# get nested list of episode.jsons for all runs
+ep_jsons = [[fp for fp in run_dir.joinpath("env_info").rglob("*.json") if fp.is_file()] for run_dir in run_dirs]
 
-env_info_df = (pd.read_json(ep_path,  convert_dates=["dates"])
+env_info_df = (pd.read_json(ep_jsons[-1][-1],  convert_dates=["dates"])
                 .set_index("dates"))
 
-rewards_ser = env_info_df["rewards"].copy().rename(index="dly_ret").rename_axis("Date")
+values_ser = env_info_df["totalValues"].copy().rename(index="total_value").rename_axis("Date")
 # %%
-indices_dir = Path.cwd().parent / "data" / "indices_performance.csv"
-indices_df = dth.load_data(indices_dir)
+indices_fp = Path.cwd().parent / "data" / "indices_performance.csv"
+indices_df = dth.load_data(indices_fp)
 *_, indices_df = dth.train_val_test_split(indices_df)
 
 stoxx50e_ser = indices_df[indices_df.index.get_level_values("symbol") == "stoxx50e"]["adjusted_close"]
@@ -41,26 +42,8 @@ dji_ser = dji_ser.pct_change()
 dji_ser = dji_ser.fillna(0)
 dji_ser = dji_ser.rename(index="dly_ret").rename_axis("Date")
 # %%
-qs.plots.snapshot(rewards_ser, title=algo_name+" Performance", mode=sum_mode)
+qs.plots.snapshot(values_ser, title=algo_name+" Performance", mode=sum_mode)
 # %%
 qs.plots.snapshot(stoxx50e_ser, title="EuroStoxx50 Performance", mode=sum_mode)
 # %%
 qs.plots.snapshot(dji_ser,title="Dow Jones Industrial Average Performance", mode=sum_mode)
-# %%
-qs.stats.comp(rewards_ser)
-# %%
-qs.stats.compsum(rewards_ser).plot()
-# %%
-rewards_ser.head()
-# %%
-returns_fb = qs.utils.download_returns('FB')
-returns_fb.head()
-# %%
-qs.stats.comp(returns_fb)
-# %%
-qs.stats.compsum(returns_fb).tail()
-# %%
-returns_fb.head()
-# %%
-returns_fb.tail()
-# %%

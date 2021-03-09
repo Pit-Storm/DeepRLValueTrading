3234bd4c7785084ed574732d470ac420270e47a8
Pit-Storm <33754750+Pit-Storm@users.noreply.github.com>
2021-03-09 20:12:40 +0000

    Extended backtseting and added ffmpeg to Dockerfile for using bar_chart_race package

--- backtesting/visualize.py
+++ backtesting/visualize.py
@@ -6,15 +6,17 @@ import pandas as pd
 from pathlib import Path
 %matplotlib inline
 import quantstats as qs
+import bar_chart_race as bcr
 # extend pandas functionality with metrics, etc.
 qs.extend_pandas()
 # %%
-algo_name = "random" # What algo to evaluate. must be the same as specified in config.py
+algo_name = "buyhold" # What algo to evaluate. must be the same as specified in config.py
 values_ser = None # timeseries with dates and rewards
 portfolio_df = None # index (Dates), [symbols, ...] (float: net amount holding), cash (float)
 indices_df = None # Timeseries with dates and daily returns
 sum_mode = "comp" # How will the growth rate be calculated? Compound or Cummulative
 
+# TODO: Do this for every algorithm in ../config.py
 algo_dir = Path.cwd().parent / "results" / algo_name.upper()
 # get all rundirs
 run_dirs = [dr for dr in algo_dir.iterdir() if dr.is_dir()]
@@ -25,6 +27,8 @@ env_info_df = (pd.read_json(ep_jsons[-1][-1],  convert_dates=["dates"])
                 .set_index("dates"))
 
 values_ser = env_info_df["totalValues"].copy().rename(index="total_value").rename_axis("Date")
+numShares_df = env_info_df["numShares"].apply(pd.Series)
+
 # %%
 indices_fp = Path.cwd().parent / "data" / "indices_performance.csv"
 indices_df = dth.load_data(indices_fp)
@@ -47,3 +51,33 @@ qs.plots.snapshot(values_ser, title=algo_name+" Performance", mode=sum_mode)
 qs.plots.snapshot(stoxx50e_ser, title="EuroStoxx50 Performance", mode=sum_mode)
 # %%
 qs.plots.snapshot(dji_ser,title="Dow Jones Industrial Average Performance", mode=sum_mode)
+# %%
+qs.reports.basic(values_ser, benchmark=stoxx50e_ser)
+# %%
+if algo_name.lower() == "buyhold":
+    bcr.bar_chart_race(df=numShares_df, 
+                    filename="numShares_race_"+ algo_name +".mp4", orientation="v",
+                    n_bars=21, fixed_order=True, period_length=200,
+                    fixed_max=True, steps_per_period=1)
+elif algo_name.lower() == "random":
+    bcr.bar_chart_race(df=numShares_df, 
+                    filename="numShares_race_"+ algo_name +".mp4", orientation="v",
+                    n_bars=21, fixed_order=True, period_length=200,
+                    fixed_max=True, steps_per_period=1)
+elif algo_name.lower() == "a2c":
+    pass
+elif algo_name.lower() == "ddpg":
+    pass
+elif algo_name.lower() == "ppo":
+    pass
+
+# TODO: Animate totalValues of every algo in one video.
+
+# TODO: Evaluate mean performance
+    # 1. Load all data into a DF with each colum is a run
+    # 2. Calculate mean value for all rows
+    # 3. Use mean value to evaluate
+
+# TODO: Generate mean of Indices
+    # Same like above
+# %%

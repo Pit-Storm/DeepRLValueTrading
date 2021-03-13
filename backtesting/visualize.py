6a885ac57df8440a396c4af13ec872434fd6afc5
Pit-Storm <33754750+Pit-Storm@users.noreply.github.com>
2021-03-13 21:37:30 +0000

    ETF Portfolio; better plots; started some investigations; New TODOOs

--- backtesting/visualize.py
+++ backtesting/visualize.py
@@ -71,68 +71,89 @@ stoxx50e_ser.index = stoxx50e_ser.index.droplevel(1)
 stoxx50e_ser = stoxx50e_ser.pct_change()
 stoxx50e_ser = stoxx50e_ser.fillna(0)
 stoxx50e_ser = stoxx50e_ser.rename(index="dly_ret").rename_axis("Date")
-stoxx50e_ser.name = "EuroStoxx50"
+stoxx50e_ser.name = "stoxx50e"
 
 dji_ser =  indices_df[indices_df.index.get_level_values("symbol") == "dji"]["adjusted_close"]
 dji_ser.index = dji_ser.index.droplevel(1)
 dji_ser = dji_ser.pct_change()
 dji_ser = dji_ser.fillna(0)
 dji_ser = dji_ser.rename(index="dly_ret").rename_axis("Date")
-dji_ser.name = "DJIA"
+dji_ser.name = "dji"
 
-# TODO: create indices portfolio
-    # percentage of invested capital as distribution of assets tradable for algo
-    # This will be the real benchmark
-    # 36/63 EU shares and 27/63 US shares
-    # stoxx50e = 36 / 63 and dji = 1 - (36/63)
-# %%
+# Create a ETF portfolio
 etf_df = pd.DataFrame(index=dates)
 etf_df = etf_df.join([dji_ser, stoxx50e_ser]).fillna(0)
+# Calculate the weighted daily returns of our ETF portfolio
+etf_df["portf_ret"] = etf_df["dji"]*(1-(36/63)) + etf_df["stoxx50e"]*(36/63)
+# Calculate the portfolio value
+etf_df["totalValue"] = etf_df["portf_ret"].add(1).cumprod().mul(1e6)
 
-# %%
-# TODO: Calculate the overall dly return of our etf portfolio
-# calculate columns with fraction of pct_change of index
-    # eg. djia_fract = dija * (1-(36/63))
-    # and stoxx50_fract = stoxx50 * (36/63)
-# calculate column with overall pct_change
-    # eg. djia_fract + stoxx_fract
-# It gets the weighted daily returns
-# To calculate a portfolio value one has to do
-# weighted_rets.add(1).mul(1e6)
-
+etf_ser = etf_df["totalValue"]
+etf_ser.name = "ETF"
+# Create a DF with all portfolio values
+portfolios_df = best_exp_mean_df.join(etf_ser)
 # %%
 # Bar chart race for Portfolio values
-bcr.bar_chart_race(df=best_exp_mean_df, dpi=330,
-                filename="best_exp_mean_totalValues_race.mp4", orientation="v",
-                fixed_order=False, period_length=200,
-                fixed_max=True, steps_per_period=1,
-                title="Portfolio Values over Time")
+# bcr.bar_chart_race(df=portfolios_df, dpi=330,
+#                 filename="best_exp_mean_totalValues_race.mp4", orientation="v",
+#                 fixed_order=False, period_length=200,
+#                 fixed_max=True, steps_per_period=1,
+#                 title="Portfolio Values over Time")
 
 # %%
+# TODO: Sort the experiments that all same commands have same ID.
 # Matrix of Mean Sharpes (Experiment times Algorithms)
 mean_sharpes_df = tests_sharpe_mean.unstack(level=["algo"])
 mean_sharpes_df.columns = mean_sharpes_df.columns.droplevel(None)
 # Best experiment sharpe is highlighted
-mean_sharpes_df.style.highlight_max(axis=0)
+mean_sharpes_df[["A2C", "DDPG", "PPO"]].style.highlight_max(axis=0)
 # %%
 # Table for evaluation of best experiments for algos and Bench with:
     # Comp. overall Return
     # Comp. annual growth rate
     # Sharpe ratio
-metrics_df = pd.DataFrame(index=["return", "CAGR", "sharpe"])
+    # Standard deviation
+metrics_df = pd.DataFrame(index=["Return", "Volatility p.a.", "CAGR", "Exp. Return p.a.", "Sharpe ratio"])
 
-for column in best_exp_mean_df.columns:
-    sharpe = qs.stats.sharpe(qs.utils.to_returns(best_exp_mean_df[column]))
-    ret = qs.stats.comp(qs.utils.to_returns(best_exp_mean_df[column]))
-    cagr = qs.stats.cagr(qs.utils.to_returns(best_exp_mean_df[column]))
-    metrics_df[column] = [ret, cagr, sharpe]
+interval = "M"
+for column in portfolios_df.columns:
+    sharpe = qs.stats.sharpe(qs.utils.to_returns(portfolios_df[column].resample(interval).last()))
+    ret = qs.stats.comp(qs.utils.to_returns(portfolios_df[column].resample(interval).last()))
+    cagr = qs.stats.cagr(qs.utils.to_returns(portfolios_df[column].resample(interval).last()))
+    vol = qs.stats.volatility(qs.utils.to_returns(portfolios_df[column].resample(interval).last()), periods=12)
+    exp_ret = qs.stats.expected_return(qs.utils.to_returns(portfolios_df[column].resample(interval).last()), aggregate="M")
+    metrics_df[column] = [ret, vol/100, cagr, exp_ret, sharpe]
+
 
 # Higlight best algorithm
 metrics_df.style.highlight_max(axis=1)
 # %%
+# Plot a linechart for all portfolios resampled to monthly means.
+portfolios_df.resample("M").last().plot()
+
+
+# %%
 # TODO: Investigations
-    # 
+#### Why DDPG wasn't effected by the drop in december 2018?
+# The news can tell us, that in december 2018 the US stock market had a historical drop
+# Let's chack this by plotting the reshaped performance of DJIA and EuroStoxx50
+etf_df[["dji", "stoxx50e"]].add(1).cumprod().resample("M").last().plot()
+# We can see a drop in DJIA, but also in EuroStoxx50...
+# What stocks did DDPG hold during that period?
+# Did it hold more or less cash during the period?
 
+# %%
+#### How could PPO make such a rise up in the end?
+# There must be one or more stocks that drived this rise.
+# For that one hase to caclulate the percentage of stockvalue on totalValue for each stock.
+# 
 
+# %%
+#### What happend with A2C performance?
+# Theory: It is not fully trained or didn't converge.
+# Reason could be the relative bad sample efficiency of A2C
+# Another theory could be, that A2C learned to reduce variance/Volatility.
+# If you reduce volatility in your portfolio, you avoid losses.
+# But in addition you avoid wins. The reason fort hat is, because standard deviation swings to both sides. Up and down.
 
 # %%
